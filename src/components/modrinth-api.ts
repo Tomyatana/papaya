0c5193c3f33eee425e9e2b98583fa5870ee1548c
Tomás Atanasoff <tomyatana@gmail.com>
2026-04-05 11:33:19 -0300

    trying to css

--- src/components/modrinth-api.ts
+++ src/components/modrinth-api.ts
@@ -1,7 +1,15 @@
 export const API_URL = "https://staging-api.modrinth.com"
 
-export async function search_projects(query: String, index = "relevance", limit = 10): Promise<ModrinthMod[]> {
-    let facet = "" // TODO: Add facet support
+export async function search_projects(
+    query: String, facets: ModrinthFacets | null = null,
+    index = "relevance", limit = 10, 
+): Promise<ModrinthMod[]> {
+    let facet: String = "" // TODO: Add facet support
+
+    if (facets != null) {
+        facet = to_facets(facets)
+    }
+
     let request = await fetch(
         `${API_URL}/v2/search?query=${query}&limit=${limit}&index=${index}${facet}`
     )
@@ -24,6 +32,47 @@ export async function search_projects(query: String, index = "relevance", limit 
     return mods
 }
 
+function to_facets(facets: ModrinthFacets): String {
+    let facet = "&facets=["
+
+    if (facets.project_type != null) {
+        facet = `${facet}["project_type=${facets.project_type}"],`
+    }
+
+    if (facets.categories != null) {
+        for (let c of facets.categories) {
+            facet = `${facet}["categories=${facets.categories}"],`
+        }
+    }
+
+    let pos = facet.lastIndexOf(',')
+    facet = facet.substring(0, pos) + facet.substring(pos+1)
+    facet += "]"
+
+    console.log(facet)
+
+
+    return facet
+}
+
+/*
+    let mut facet = "&facets=[[\"project_type=mod\"],".to_string();
+    let facet_1 = "[\"";
+    let facet_2 = "\"],";
+    for element in categories.iter() {
+        let filter = format!("{}categories:{}{}", facet_1, element, facet_2);
+        facet.push_str(&filter);
+    }
+    if !game_version.is_empty() {
+        let version = format!("{}versions:{}{}", facet_1, game_version, facet_2);
+        facet.push_str(&version);
+    }
+    facet.push_str(facet_2);
+    facet = remove_last_char(&facet, ',');
+    facet = remove_last_char(&facet, '"');
+    remove_last_char(&facet, ',')
+*/
+
 export interface ModrinthMod { // TODO: Add more fields
     title: String,
     desc: String | null,
@@ -31,4 +80,10 @@ export interface ModrinthMod { // TODO: Add more fields
     downloads: number,
 
     icon: String | null
+}
+
+export interface ModrinthFacets {
+    project_type: String | null,
+    categories: String[] | null,
+    version: String | null,
 }
